de77081484c1588f4ee8a201fa57951bf87b07a5
ahmedfahad04 <bsse1204@iit.du.ac.bd>
2022-01-19 04:58:18 +0600

    file updated

--- Assignments/activity_selection_problem.cpp
+++ Assignments/activity_selection_problem.cpp
@@ -12,15 +12,17 @@ bool compsec(const pair<int, int> &a, const pair<int, int> &b){
 vector<pair<int,int>> activity_selector (vector<pair<int,int>> data, int initial_point){
 
 	vector<pair<int,int>> result;
-
 	result.push_back(data[initial_point]);
-	int k=initial_point;
 
-	for(int m=initial_point+2; m<data.size(); m++){
+	int k = initial_point;
+
+	for(int m=initial_point + 2; m < data.size(); m++){
+
 		if(data[m].start >= data[k].finish){
 			result.push_back(data[m]);
 			k = m;
 		}
+
 	}
 
 	return result;
@@ -34,7 +36,9 @@ int main(){
 	freopen("output.txt","w",stdout);   //file output.txt is opened in writing mode i.e "w"
 	#endif
 
-	vector<pair<int, int>> p, ans;
+	vector<pair<int, int>> p, ans, temp;
+
+	vector<vector<pair<int, int>>> optimal_solutions;
 
 	int n;
 
@@ -50,23 +54,38 @@ int main(){
 
 	sort(p.begin(), p.end(), compsec);
 
-
-
-	// cout << "Before: " << endl;
-
-	// for(int i=0; i<n; i++){
-	// 	cout << "Start: " << p[i].start << ", End: " << p[i].finish << endl;
-	// }
+	int opt_value = 0;
 
 	for(int i=0; i<n; i++){
 
 		ans = activity_selector(p, i);
 
 		for(int i=0; i<ans.size(); i++){
-			cout << "Start: " << ans[i].start << ", End: " << ans[i].finish << endl;
+			temp.push_back(mk(ans[i].start, ans[i].finish));
+			//cout << "Start: " << ans[i].start << ", End: " << ans[i].finish << endl;
 		}
 
-		cout << "Total Work Done: " << ans.size() << "\n\n";
+		optimal_solutions.push_back(temp);
+		temp.clear();
+
+		if(opt_value<=ans.size()) opt_value = ans.size();
+
+		// cout << "Total Work Done: " << ans.size() << "\n\n";
+	}
+
+	int sol = 1;
+	for(int i=0; i<n; i++){
+
+		if(optimal_solutions[i].size() == opt_value){
+
+			cout << "Optimal solution: " <<  sol++ << endl;
+			for(int j=0; j<opt_value; j++){
+				cout << "Start: " << optimal_solutions[i][j].start << ", End: " << optimal_solutions[i][j].finish << endl;
+			}
+
+			cout << endl;
+
+		}
 	}
 
 }
