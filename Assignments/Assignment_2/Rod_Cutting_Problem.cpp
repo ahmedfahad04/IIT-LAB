dfd38fdd2e73f8ee54830c76f95b89d84f1fa1c5
Istiaq Ahmed Fahad <60494055+ahmedfahad04@users.noreply.github.com>
2022-02-01 07:43:41 +0600

    Update Rod_Cutting_Problem.cpp

--- Assignments/Assignment_2/Rod_Cutting_Problem.cpp
+++ Assignments/Assignment_2/Rod_Cutting_Problem.cpp
@@ -1,47 +1,61 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int rod_cut(int p[], int n){
-    
-    int r[100];
+const int INF = -1e5 + 5;
+
+int s[100];
+int r[100];
+
+void rod_cut(int p[], int n)
+{
 
     r[0] = 0;
 
-    for(int j = 1; j <= n; j++){
-        int q = -10000;
+    for (int j = 1; j <= n; j++)
+    {
+        int q = INF;
 
-        for(int i=1; i <= j; i++){
-            q = max(q, p[i]+r[j-i]);
-            cout << "id: " << i << endl;
+        for (int i = 1; i <= j; i++)
+        {
+
+            if (q < p[i - 1] + r[j - i])
+            {
+                q = p[i - 1] + r[j - i];
+                s[j] = i;
+            }
         }
 
         r[j] = q;
-        cout << "R: " << r[j] << endl;
-        cout << endl;
     }
-
-    // for(int i=1; i<n; i++){
-    //     cout << "FINAL: " << r[i] << endl;
-    // }
-
-    return r[n];
 }
 
-int main(){
+void Print_Cut_Rod_Solution(int p[], int n)
+{
+
+    while (n > 0)
+    {
+        cout << s[n] << " ";
+        n -= s[n];
+    }
+}
+
+int main()
+{
 
     freopen("in.txt", "r", stdin);
-    freopen("out.txt", "w", stdout);
+    //freopen("out.txt", "w", stdout);
 
     int len;
     //cout << "Enter the length to be cut: ";
     cin >> len;
 
-    int price[] = { -1, 2, 5, 9, 6};
+    int price[] = {3, 5, 8, 9, 10, 17, 17, 20};
+    int n = sizeof(price) / sizeof(price[0]);
 
-    cout << "\n\n";
+    rod_cut(price, n);
+    cout << "MAX revenue: " << r[len] << endl;
 
-    int ans = rod_cut(price, len);
-
-    cout << "MAX revenue: " << ans << endl;
-
+    cout << "Optimal Cutting: ";
+    Print_Cut_Rod_Solution(price, len);
+    cout << endl;
 }
