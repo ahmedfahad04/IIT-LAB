2abc7da6fe13c3db0b5543a8cb66761ccd92e827
ahmedfahad04 <bsse1204@iit.du.ac.bd>
2022-02-08 11:15:28 +0600

    Coin Changed Done

--- Assignments/Assignment_3/test.cpp
+++ Assignments/Assignment_3/test.cpp
@@ -1,10 +1,10 @@
 #include <bits/stdc++.h>
-#define N 100
 using namespace std;
-
-int dp[N][N];
 int count(int p[], int n, int m)
 {
+
+    int dp[n + 1][m];
+
     for (int i = 0; i < m; i++)
     {
         dp[0][i] = 1;
@@ -31,42 +31,45 @@ int count(int p[], int n, int m)
 }
 int min_coins(int p[], int m, int n)
 {
+    int dp[n][m + 1];
 
-    for (int i = 0; i <= m; i++)
+    for (int i = 0; i < n; i++)
     {
-        dp[0][i] = 1;
+        dp[i][0] = 0;
     }
 
-    int minc = INT_MAX;
+    // just added this 42th line to initialize the first row with +infinity
+    // so that we can compare the 1st row with 0th row to get the min value
+    for (int i = 1; i <= m; i++) dp[0][i] = INT_MAX;
 
-    for (int i = 0; i < m; i++)
+
+    for (int i = 0; i < n; i++)
     {
-        for (int j = 1; j <= n; j++)
+        for (int j = 1; j <= m; j++)
         {
-            if (p[i] >= j) // coin er value total er theke beshi, copy from above
-            {            
-                dp[i][j] = min(minc, dp[i][j - p[i]] + 1); // minumum of guy in the top or go back p[i] steps to the left+1
+            if (p[i] > j) // coin er value total er theke beshi, copy from above
+            {
+                dp[i][j] = dp[i - 1][j];
             }
+            else
+            {
+                dp[i][j] = min(dp[i - 1][j], dp[i][j - p[i]] + 1); // minumum of guy in the top or go back p[i] steps to the left+1
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= m; j++)
+        {
             cout << dp[i][j] << " ";
         }
-
-        
         cout << endl;
     }
-
-    // for (int i = 0; i < n; i++)
-    // {
-    //     for (int j = 0; j <= m; j++)
-    //     {
-    //         cout << dp[i][j] << " ";
-    //     }
-    //     cout << endl;
-    // }
-    return dp[n-1][m];
+    return dp[n - 1][m];
 }
-
 int main()
 {
+
     freopen("in.txt", "r", stdin);
 
     int val; // total value
