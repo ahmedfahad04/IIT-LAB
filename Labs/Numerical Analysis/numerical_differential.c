3f0fcf7a9853837b30c9a90d7aeaa55919b856dd
ahmedfahad04 <bsse1204@iit.du.ac.bd>
2022-03-10 15:26:02 +0600

    numerical_differentiation updated

--- Labs/Numerical Analysis/numerical_differential.c
+++ Labs/Numerical Analysis/numerical_differential.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
-#define N 5
+#include <math.h>
 
-double x[N] = {0, 1, 2, 3, 4};
-double f[N] = {1, 2, 2.5, 2.8, 3.0};
+#define N 300
+#define func(x) (((x)*(x)*(x)) + (4*(x)*(x)) - 10)
+#define df(x) ((3*(x)*(x)) + (8*(x)))
+
+double x[N+1];
+double f[N+1];
+double calculated_df[N+1];
+double dynamic_df[N+1];
+
 
 double ford(int x2, int x1)
 {
@@ -22,12 +29,53 @@ double back(int x1, int x0)
 int main()
 {
 
-    printf("x: %lf      f: %lf      df: %lf\n", x[0], f[0], ford(1, 0));
 
-    for (int i = 1; i < N - 1; i++)
-    {
-        printf("x: %lf      f: %lf      df: %lf\n", x[i], f[i], cent(i-1,i+1));
+    // variable declaration
+    double a = 0.0;
+    double b = 10.0;
+    double max_diff = -1000009;
+    dynamic_df[0] = ford(1, 0);
+    dynamic_df[0] = back(N, N-1);
+    x[0] = 0;
+    f[0] = func(0);
+
+
+    // build x
+    for(int i=1; i<=N; i++){
+        x[i] = x[i-1] + (b-a)/N;  // making the array of x
+        f[i] = func(x[i]);  // making the array of f
+        calculated_df[i] = df(x[i]);
     }
 
-    printf("x: %lf      f: %lf      df: %lf\n", x[N - 1], f[N - 1], back(N-1, N - 2));
+
+    // define the max difference
+    double diff = fabs(calculated_df[0] - ford(1, 0));
+    if(max_diff <= diff) max_diff = diff;
+
+    diff = fabs(calculated_df[N] - back(N, N-1));
+    if(max_diff <= diff) max_diff = diff;
+        
+  
+
+    // printing the values as table formate
+    printf("x             f             df               calculated df\n");
+    printf("--------      ---------     ---------        --------\n");
+    printf("%lf      %lf      %lf        %lf\n", x[0], f[0], ford(1, 0), calculated_df[0]);
+
+    for (int i = 1; i < N; i++)
+    {
+        // calculating the dfs...
+        dynamic_df[i] = cent(i-1,i+1);
+        double calc_df = calculated_df[i];
+        printf("%lf      %lf      %lf          %lf\n", x[i], f[i], dynamic_df[i], calc_df);
+        
+        // calculating max differences
+        diff = fabs(calc_df - dynamic_df[i]);
+        if(max_diff <= diff) max_diff = diff;
+    }
+
+    printf("%lf      %lf      %lf          %lf\n", x[N], f[N], back(N, N-1), calculated_df[N]);
+
+    printf("\nMAX DIFFERECE: %lf\n", max_diff);
+
 }
