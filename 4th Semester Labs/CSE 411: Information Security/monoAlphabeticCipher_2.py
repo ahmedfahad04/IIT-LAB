25634636fde3316bb93842fae697b59544d40752
ahmedfahad04 <bsse1204@iit.du.ac.bd>
2022-06-22 11:24:49 +0600

    Information Security Lab practice added

---
+++ 4th Semester Labs/CSE 411: Information Security/monoAlphabeticCipher_2.py
@@ -0,0 +1,40 @@
+def encrypt(text, key):
+    modifiedText = ""
+    
+    for i in range(len(text)):
+        if i%2 == 0:
+            modifiedText += chr((ord(text[i]) + key)%127)
+        else: 
+            modifiedText += chr((ord(text[i]) + key + 12)%127)
+                    
+    return modifiedText  
+        
+
+def decrypt(modifiedText, key):
+    plainText = ""
+    
+    for i in range(len(modifiedText)):
+        if i%2 == 0:
+            plainText += chr((ord(modifiedText[i]) - key)%127)
+            # plainText += chr((ord(modifiedText[i]) + 63 - key)%127)
+        else: 
+            plainText += chr((ord(modifiedText[i]) - key - 12)%127)
+            # plainText += chr((ord(modifiedText[i]) + 63 - key - 12)%95+32)
+            
+            
+    return plainText  
+    
+
+def main():
+    data = input("Enter your text: ")
+    key = int(input("Enter your key: "))
+    
+    ecnryptedText = encrypt(data, key)  
+    print("Encrypted Text:", ecnryptedText)
+     
+    decryptedText = decrypt(ecnryptedText, key)
+    print("Decrypted Text:", decryptedText)
+    
+if __name__ == '__main__':
+    main()
+    
