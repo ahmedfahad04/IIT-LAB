7b3ed9c52104c02a52b85a62b517e6e989ddfd37
ahmedfahad04 <bsse1204@iit.du.ac.bd>
2022-09-02 08:22:45 +0600

    Done with SHA512

--- 4th Semester Labs/ISTestCodes/bit.py
+++ 4th Semester Labs/ISTestCodes/bit.py
@@ -23,7 +23,18 @@
 # # print("Modified HEX: ", hex(int(x, 2)))
 # # print(int(x,2))
 # print("S: ", (format(sigma_0(n), '064x')))
-def ror(n, d): return format((n >> d) | (n << (64 - d)), '016x')[-16:]
+ror=lambda x,y :((x >> y) | (x << (64-y))) & 0xFFFFFFFFFFFFFFFF
 def shr(n, d): return n >> d
-def sigma_1(x): return int(ror(x, 19), 16) ^ int(ror(x, 61), 16) ^ shr(x, 6)
-print(ror(11, 1))
+def sigma_0(x): return ror(x, 1) ^ ror(x, 8) ^ shr(x, 7)
+def sigma_1(x): return ror(x, 19) ^ ror(x, 61) ^ shr(x, 6)
+def sigma_a(x): return ror(x, 28) ^ ror(x, 34) ^ ror(x, 39)
+
+def ch(x, y, z): return (x & y) ^ ((~x) & z)
+def maj(x, y, z): return (x & y) ^ (x & z) ^ (y & z)
+
+# print((bin(ror(0b1011, 4))))
+# print(len(hex(sigma_1(0b1011))))
+
+a = hex(95)
+print(a)
+print(bin(95))
