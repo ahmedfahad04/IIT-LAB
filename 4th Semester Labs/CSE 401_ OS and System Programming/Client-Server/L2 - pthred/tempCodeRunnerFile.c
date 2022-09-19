e1c3c61546d7cfd48e9f0dd13b674e62e7c33d89
ahmedfahad04 <bsse1204@iit.du.ac.bd>
2022-09-20 02:42:07 +0600

    4th Semester Labs have been replaced

--- 4th Semester Labs/CSE 401_ OS and System Programming/Client-Server/L2 - pthred/tempCodeRunnerFile.c
+++
@@ -1,8 +0,0 @@
-// start child thread
-        if (pthread_create(&thread_id, NULL, dostuff, (void *)&new_sockfd) < 0)
-        {
-            perror("ERROR create thread");
-            exit(1);
-        }
-
-        pthread_detach(thread_id); // don't track it
